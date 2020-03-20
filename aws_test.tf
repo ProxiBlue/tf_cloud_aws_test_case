afbde75a550b9454cc8381f7cbf829b480af65d9
Lucas <lucas.vanstaden@enjo.com.au>
2020-03-20 10:21:19 +0800

    wip - add a second box to provision

--- aws_test.tf
+++ aws_test.tf
@@ -10,6 +10,17 @@ resource "aws_instance" "linux" {
   }
 }
 
+resource "aws_instance" "linuxbox2" {
+  ami           = "ami-099fd1de9981c9ef5"
+  instance_type = "t2.micro"
+  availability_zone = "ap-southeast-2a"
+  user_data = data.template_file.installation_template.rendered
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
 data "template_file" "installation_template" {
   template = "${file("install.tpl")}"
   vars = {
